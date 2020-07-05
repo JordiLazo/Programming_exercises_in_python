23d1d45479b754fd1e1d04454b665783aa6b7b71
Jordi Lazo <jordilazo94@gmail.com>
2020-07-05 11:18:09 +0200

    Create hola.py

---
+++ The Complete Python 3 Course: Beginner to Advanced!/hola.py
@@ -0,0 +1,11 @@
+print("Hello")
+print("Jordi lAzo")
+print("5" + "6")
+x = int("900") + int("900")
+
+x = 'She said: "Don\'t do that."'
+print(x)
+y = "She said: \"Callate carajo!\""
+print(y)
+
+print("Prueba del shortcast")
